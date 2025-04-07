5d130325ac18afdbc055a9e314b0e25e89f1143e
Vlad Kalashnikov <kalashnikovv537@gmail.com>
2025-04-07 21:02:19 +0500

    aruments parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::{Command, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("Rget")
+        .version("0.1.0")
+        .author("Kalashnikov Vladislav")
+        .about("wget clone written in Rust")
+        .arg(
+            Arg::new("URL")
+                .required(true)
+                .index(1)
+                .help("url to download"),
+        )
+        .get_matches();
+
+    let url = matches.get_one::<String>("URL").unwrap();
+    println!("{}", url);
 }
