04ae5717cc1a9436956f12e27e529ed16edd472f
Vlad Kalashnikov <kalashnikovv537@gmail.com>
2025-04-23 15:17:27 +0500

    added progres bar

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Command, Arg};
+use indicatif::{ProgressBar, ProgressStyle};
 
 fn main() {
     let matches = Command::new("Rget")
@@ -15,4 +16,30 @@ fn main() {
 
     let url = matches.get_one::<String>("URL").unwrap();
     println!("{}", url);
+}
+
+fn create_progress_bar(quiet_mode: bool, msg: &str, length: Option<u64>) -> ProgressBar {
+    let bar = if quiet_mode {
+        ProgressBar::hidden()
+    } else {
+        match length {
+            Some(len) => ProgressBar::new(len),
+            None => ProgressBar::new_spinner(),
+        }
+    };
+
+    bar.set_message(msg.to_string());
+
+    if let Some(_) = length {
+        bar.set_style(
+            ProgressStyle::default_bar()
+                .template("{msg} {spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/.blue}] {bytes}/{total_bytes} eta: {eta}")
+                .unwrap()
+                .progress_chars("=> "),
+        );
+    } else {
+        bar.set_style(ProgressStyle::default_spinner());
+    }
+
+    bar
 }
